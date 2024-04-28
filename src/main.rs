a671a50b63b95de4e148aaef8ca6097816bf6cc8
l0raxeo_mac <fuzedcable@gmail.com>
2024-04-28 16:23:53 -0400

    full code thingy without encryption

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::char::{self};
 use std::fs::File;
 use std::io::prelude::*;
 
-fn save(sequence: &str) -> std::io::Result<()> {
+fn save(code: &str) -> std::io::Result<()> {
     let mut file = File::create("snoopster.txt")?;
-    file.write_all(sequence.as_bytes())?;
+    file.write_all(code.as_bytes())?;
     Ok(())
 }
 
@@ -16,7 +16,7 @@ fn generate() -> String {
     let mut slots_available: [bool; 14] = [true; 14];
     let mut cur_slot: i32 = rng.gen_range(0..14);
 
-    for _i in 0..14 {
+    for _ in 0..sequence.len() {
         while slots_available[cur_slot as usize] == false {
             cur_slot = rng.gen_range(0..14);
         }
@@ -32,14 +32,16 @@ fn generate() -> String {
         }
     }
 
-    for i in 0..sequence.len() {
-        print!("{}", sequence[i]);
+    let mut code: String = sequence[0].to_string();
+
+    for i in 1..sequence.len() {
+        code.push(sequence[i as usize]);
     }
 
-    "blah".to_string()
+    code
 }
 
 fn main() {
-    let sequence: String = generate();
-    let _ = save(&sequence);
+    let code: String = generate();
+    let _ = save(&code);
 }
